91559858d1508a533546def34fd7fe3732e0c6cc
termick <christos_karapiperis@yahoo.gr>
2014-05-21 00:17:54 +0300

    working version
    working version, needs more comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+library(MASS)
+makeCacheMatrix <- function(x=matrix()){
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL       
+        }
+        get<-function() x
+        setinv<-function(ginv) m<<-ginv
+        getinv<-function() m
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve<-function(x, ...){
+        m<-x$getinv()
+        if(!is.null(m)){
+                message("getting cache data")
+                return(m)
+## Return a cache matrix that is the inverse of 'x'                          
+        }
+        data<-x$get()
+        m<-ginv(data, ...)
+        x$setinv(m)
+        m
+## Return a matrix that is the inverse of 'x'          
 }
