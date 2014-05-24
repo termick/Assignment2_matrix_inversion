a6d4b86b2e5adf48794d5764564424d0f09baa3b
termick <christos_karapiperis@yahoo.gr>
2014-05-24 13:05:45 +0300

    Working file with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,47 @@
-## Put comments here that give an overall description of what your
+## Two functions to create calculate the inverse matrix. 
 ## functions do
 
-## Write a short comment describing this function
-library(MASS)
+## The first function, makeCacheMAtrix creates a special matrix, which is really a list containing a function to
+
+
+
+library(MASS) #load MASS library to use the matrix inverse function
 makeCacheMatrix <- function(x=matrix()){
         m<-NULL
-        set<-function(y){
+        set<-function(y){     #set the value of the matrix
                 x<<-y
                 m<<-NULL       
-        }
-        get<-function() x
-        setinv<-function(ginv) m<<-ginv
-        getinv<-function() m
+        }lil
+        get<-function() x      #get the value of the matrix
+        setinv<-function(ginv) m<<-ginv    #set the value of the inverse matrix
+        getinv<-function() m       #get the value of the inverse matrix
         list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-## Write a short comment describing this function
+## The second function  computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 
 
 cacheSolve<-function(x, ...){
+        
         m<-x$getinv()
         if(!is.null(m)){
                 message("getting cache data")
                 return(m)
-## Return a cache matrix that is the inverse of 'x'                          
+# Return a cache matrix that is the inverse of 'x'                          
         }
         data<-x$get()
         m<-ginv(data, ...)
         x$setinv(m)
         m
-## Return a matrix that is the inverse of 'x'          
+##Return a matrix that is the inverse of 'x'          
 }
+
+#input example:
+#z<-matrix(1:4, nrow=2, ncol=2)
+#a<-makeMatrix(x=z)
+#a$get()
+#cacheSolve(a)
+
+
